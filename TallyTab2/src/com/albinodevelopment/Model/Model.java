d0d8a4bec4095353ea66094808ab1976debdf70e
Connor <connorwilliams.home@gmail.com>
2018-12-15 16:09:21 +1100

    Initial Commit

---
+++ TallyTab2/src/com/albinodevelopment/Model/Model.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.albinodevelopment.Model;
+
+/**
+ *
+ * @author conno
+ */
+public class Model {
+    
+}
