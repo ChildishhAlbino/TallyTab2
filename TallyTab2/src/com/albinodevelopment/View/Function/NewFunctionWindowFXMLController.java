28ca80152a211ce406a872e97384f319b72ce5ca
Connor <connorwilliams.home@gmail.com>
2018-12-22 00:02:30 +1100

    [Connor] Add new function window FXML and empty controller

---
+++ TallyTab2/src/com/albinodevelopment/View/Function/NewFunctionWindowFXMLController.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.albinodevelopment.View.Function;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author conno
+ */
+public class NewFunctionWindowFXMLController implements Initializable {
+
+    @FXML
+    private TextField functionNameTF;
+    @FXML
+    private TextField functionLimitTF;
+    @FXML
+    private Label selectedMenuLabel;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void onSelectMenuButtonAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void onSubmitButtonAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void onCancelButtonAction(ActionEvent event) {
+    }
+    
+}
