2f9bf465ad73bf7cf5e18262976533fdeeeb3d81
Connor <connorwilliams.home@gmail.com>
2018-12-27 07:46:50 +1100

    [Connor] Add function menu selection

--- TallyTab2/src/com/albinodevelopment/View/Function/NewFunctionWindowController.java
+++ TallyTab2/src/com/albinodevelopment/View/Function/NewFunctionWindowController.java
@@ -39,7 +39,9 @@ public class NewFunctionWindowController extends Window implements Initializable
     private TextField functionLimitTF;
     @FXML
     private Label selectedMenuLabel;
-
+    
+    private String menuDirectory = "";
+    
     /**
      * Initializes the controller class.
      */
@@ -54,6 +56,7 @@ public class NewFunctionWindowController extends Window implements Initializable
         if (filePath != null) {
             selectedMenuLabel.setText(filePath);
         }
+        menuDirectory = filePath;
     }
 
     @FXML
@@ -61,9 +64,9 @@ public class NewFunctionWindowController extends Window implements Initializable
 
         String functionTitle = (functionNameTF.getText().equals("") ? null : functionNameTF.getText());
         String functionLimit = (functionLimitTF.getText().equals("") ? "" : functionLimitTF.getText());
-
+        
         handle(new ViewCommand.PassToControllerCommand(
-                new ControllerCommand.ValidateNewFunctionCommand(functionTitle, functionLimit, FileIO.getMenuDirectory() + "//TestMenu.xml", this)));
+                new ControllerCommand.ValidateNewFunctionCommand(functionTitle, functionLimit, menuDirectory, this)));
 
     }
 
